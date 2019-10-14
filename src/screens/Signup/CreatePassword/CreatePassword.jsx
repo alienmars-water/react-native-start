88e63202ce47c45a84fa18b34b0326163f7f016f
Walter Wang <alienmars.water@outlook.com>
2019-10-14 23:22:42 +0800

    flexboxes

--- src/screens/Signup/CreatePassword/CreatePassword.jsx
+++ src/screens/Signup/CreatePassword/CreatePassword.jsx
@@ -7,14 +7,17 @@ export default class CreatePassword extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={[styles.box,{ backgroundColor: 'green'}]}>
+                <View style={[styles.box, styles.boxGreen]}>
                     <Text>Box Green</Text>
                 </View>
-                <View style={[styles.box,{ backgroundColor: 'yellow' }]}>
+                <View style={[styles.box, styles.boxYellow]}>
                     <Text>Box Yellow</Text>
                 </View>
-                <View style={[styles.box, { backgroundColor: 'red'}]}>
+                <View style={[styles.box, styles.boxRed]}>
                     <Text>Box Red</Text>
+                </View>
+                <View style={[styles.box, styles.boxBlue]}>
+                    <Text>Box Blue</Text>
                 </View>
             </View >
         )
--- src/screens/Signup/CreatePassword/CreatePassword.styles.js
+++ src/screens/Signup/CreatePassword/CreatePassword.styles.js
@@ -3,12 +3,25 @@ import { StyleSheet, Platform } from 'react-native'
 export default StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
         alignItems: 'flex-start',
         marginTop: 30
     },
     box: {
         height: 100,
         width: 100
+    },
+    boxGreen: {
+        backgroundColor: 'green'
+    },
+    boxYellow: {
+        backgroundColor: 'yellow'
+    },
+    boxRed: {
+        backgroundColor: 'red'
+    },
+    boxBlue: {
+        backgroundColor: 'blue'
     }
 })
