141fcf861685cc899d9a1487325f342ec8075422
Walter Wang <alienmars.water@outlook.com>
2020-01-12 22:11:17 +0800

    animation path

--- App.jsx
+++ App.jsx
@@ -28,10 +28,10 @@ import { CreateUserName, CreatePassword } from './src/screens/Signup'
 import { Homepage, Authorise } from './src/screens/MyInfo'
 import { Robot } from './src/screens/Robot'
 import { Circles } from './src/screens/Svg'
-import { MovingBox, ClickSquare, ScrollHeader } from './src/screens/Animation'
+import { MovingBox, ClickSquare, ScrollHeader, PathPaint } from './src/screens/Animation'
 
 const App = () => {
-  return <ScrollHeader />
+  return <PathPaint />
 };
 
 const styles = StyleSheet.create({
--- src/screens/Animation/index.js
+++ src/screens/Animation/index.js
@@ -1,5 +1,6 @@
 import MovingBox from './MovingBox'
 import ClickSquare from './ClickSquare'
 import ScrollHeader from './ScrollHeader'
+import PathPaint from './PathPaint'
 
-export { MovingBox, ClickSquare, ScrollHeader }
+export { MovingBox, ClickSquare, ScrollHeader, PathPaint }
